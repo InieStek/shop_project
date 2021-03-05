cf00b5face8b79f1452401de3ce9830512550c88
Michal <michal001d9@gmail.com>
2021-03-05 11:17:02 +0100

    whole page

---
+++ Products/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+# Create your models here.
+
+class Producent(models.Model):
+    def __str__(self):
+        return self.name
+
+    name = models.CharField(max_length=60)
+    descryption = models.TextField(blank=True)
+
+    class Meta:
+        verbose_name = "Producent"
+        verbose_name_plural = "Producers"
+
+class Category(models.Model):
+    def __str__(self):
+        return self.name
+
+    name = models.CharField(max_length=30)
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Category"
+
+class Products(models.Model):
+    category = models.ForeignKey(Category, null=True, blank = True, on_delete=models.CASCADE)
+    producent = models.ForeignKey(Producent, on_delete=models.CASCADE,null=True)
+    name = models.CharField(max_length=100)
+    descryption = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=12,decimal_places=2)
+
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Products"
+        verbose_name_plural = "Products"
+
+
+
---
+++ Products/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from .models import Products, Category
+from django.http import HttpResponse
+# Create your views here.
+
+def index(request):
+    question = Products.objects.all()
+    category = Category.objects.all()
+    data = {'category' : category}
+    return render(request, 'template.html', data)
+
+def category(request, id):
+    category_user = Category.objects.get(pk=id)
+    return HttpResponse(category_user)
+
+def products(request, id):
+    products_user = Products.objects.get(pk=id)
+    inscryption = "<h1>" +  str(products_user) + "</h1>" + \
+        "<p></p>" +  str(products_user.descryption)  + "</p>" + \
+        "<p>" +  str(products_user.price) + "</p>"
+    return HttpResponse(inscryption)
