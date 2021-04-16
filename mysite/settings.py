beedc2579f3f3716b1785d015995b5c93c82480d
Michal <michal001d9@gmail.com>
2021-04-16 17:24:33 +0200

    shop-project

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,6 +122,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = ['static']
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = 'media'
+
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
--- mysite/urls.py
+++ mysite/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from Products.views import *
+from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import url
 urlpatterns = [
@@ -27,4 +28,4 @@ urlpatterns = [
     path(r'^signup/$', signup, name='signup'),
     path('accounts/',include('django.contrib.auth.urls')),
 
-    ]
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
